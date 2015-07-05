2cf339d7be85f2586867a280bef18be9515029da
Ankk4 <antero.juutinen@gmail.com>
2015-07-06 02:16:06 +0300

    perkele = minspeed
    perkele = minspeed

--- public/js/game.js
+++ public/js/game.js
@@ -46,13 +46,14 @@ function create() {
 
 	//initial velocity
     ball.body.angularVelocity = 400;
-	ball.body.velocity.set(400, -200);
+	ball.body.velocity.set(100, -150);
 
 	//collisions
 	game.physics.arcade.overlap(ball, bullets, bulletHitBall, null, this);
 	game.physics.arcade.overlap(ball, player, ballHitPlayer)
 }
 
+var perkele = 5;
 function update() {
 	player.animations.play('idle', 5);
 
@@ -73,9 +74,16 @@ function update() {
 
 	if (ball.body.blocked.left) {
 		ball.body.angularVelocity = 400;
+		perkele *= -1;
 	} else if (ball.body.blocked.right) {
 		ball.body.angularVelocity = -400;
+		perkele *= -1;
+	} else if (ball.body.blocked.down) {
+		ball.body.velocity.y -= 20;
 	}
+
+	//perkele = min speed - parempi tapa tehä tämä?
+	ball.body.x += perkele;
 
 	game.debug.body(ball);
 	game.debug.bodyInfo(ball, 32, 32);
